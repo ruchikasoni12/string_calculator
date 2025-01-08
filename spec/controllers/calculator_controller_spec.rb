c3b9d4b3c7f48593a07e7d03745c793e160ec0dd
Rubydev <ruchikasoni740@gmail.com>
2025-01-09 00:18:21 +0530

    if string num is given so it will return num it self

--- spec/controllers/calculator_controller_spec.rb
+++ spec/controllers/calculator_controller_spec.rb
@@ -8,5 +8,12 @@ RSpec.describe CalculatorsController, type: :controller do
 	 			expect(JSON.parse(response.body)["result"]).to eq(0)
 	 		end
 	 	end
+	 	context "when number is provided" do
+	 		it "returns the number itself" do
+	 			post :calculate_sum, params: { numbers: "5" }
+	 			expect(response).to have_http_status(:ok)
+	 			expect(JSON.parse(response.body)["result"]).to eq(5)
+	 		end
+	 	end
     end
 end
