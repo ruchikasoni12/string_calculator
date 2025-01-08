1635f30f05bb91a923aa42bf7ca9545229e66352
Rubydev <ruchikasoni740@gmail.com>
2025-01-09 00:08:10 +0530

    if number is empty so result will be 0

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -1,5 +1,8 @@
 class CalculatorsController < ApplicationController
  def calculate_sum
-    
+ 	num = params[:numbers]
+ 	if num.empty?
+      render json: {result: 0}
+    end
   end
 end
--- spec/controllers/calculator_controller_spec.rb
+++ spec/controllers/calculator_controller_spec.rb
@@ -3,7 +3,9 @@ RSpec.describe CalculatorsController, type: :controller do
    describe "POST #calculate_sum" do
 	 	context "when no numbers are provided" do
 	 		it "returns 0" do
-	 			
+	 			post :calculate_sum, params: {numbers: ""}
+	 			expect(response).to have_http_status(:ok)
+	 			expect(JSON.parse(response.body)["result"]).to eq(0)
 	 		end
 	 	end
     end
