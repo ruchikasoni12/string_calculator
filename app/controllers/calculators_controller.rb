5f587f6011c28bd4903e98ad2411a10b38e562ad
Rubydev <ruchikasoni740@gmail.com>
2025-01-09 00:02:53 +0530

    I am following the TDD practice

---
+++ app/controllers/calculators_controller.rb
@@ -0,0 +1,5 @@
+class CalculatorsController < ApplicationController
+ def calculate_sum
+    
+  end
+end
--- spec/controllers/calculator_controller_spec.rb
+++ spec/controllers/calculator_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 RSpec.describe CalculatorsController, type: :controller do
    describe "POST #calculate_sum" do
-	 	
+	 	context "when no numbers are provided" do
+	 		it "returns 0" do
+	 			
+	 		end
+	 	end
     end
 end
